d9b2deb178325d52e367dce9204cc387548abd6c
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-09-10 10:54:31 +0200

    Mise à jour du projet par OpenClassrooms

--- src/app/features/rentals/components/list/list.component.ts
+++ src/app/features/rentals/components/list/list.component.ts
@@ -6,18 +6,21 @@ import { RentalsService } from '../../services/rentals.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
-
   public rentals$ = this.rentalsService.all();
 
   constructor(
     private sessionService: SessionService,
     private rentalsService: RentalsService
-  ) { }
+  ) {}
 
   get user(): User | undefined {
     return this.sessionService.user;
+  }
+
+  getImageUrl(picture: string): string {
+    return picture.startsWith('/uploads/') ? picture : `/uploads/${picture}`;
   }
 }
